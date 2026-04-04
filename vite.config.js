c5b2248f21552a6efe8c56b943e0faa777a72f49
daile269 <daile2692003@gmail.com>
2026-04-04 12:21:51 +0700

    update port

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5180,
-    open: true,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      port: parseInt(env.VITE_PORT) || 5180,
+      open: true,
+    },
+  };
 });
